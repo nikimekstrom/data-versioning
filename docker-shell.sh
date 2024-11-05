cfd588dfef522092ff7ca0ede464584637296e3c
Niki Ekstrom <nikiekstrom@g.harvard.edu>
2024-11-05 11:33:45 -0500

    initial dataset

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,9 +4,9 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
+export GCS_BUCKET_NAME="cheese-app-data-versioning-niki"
+export GCP_PROJECT="ac215-t1-435201"
+export GCP_ZONE="us-central1"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
 
